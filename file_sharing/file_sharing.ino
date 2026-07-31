ebe7280d8264b6710a1411d528cf2d59e6d56b1d
dennisdebel <dennisdebel@gmail.com>
2026-08-01 03:26:37 +0800

    fixed some bugs, now supports 100kb files
    maybe larger! we need a proper queue

--- file_sharing/file_sharing.ino
+++ file_sharing/file_sharing.ino
@@ -1,7 +1,6 @@
-//#include <ESP8266mDNS.h>
-#include "./DNSServer.h" // Dns server, needed for mdns!
+#include "./DNSServer.h"
 #include "painlessMesh.h"
-
+#include <vector>
 #include <Arduino.h>
 #include <LittleFS.h>
 
@@ -13,33 +12,60 @@
 
 #include <ESPAsyncWebServer.h>
 
+
 // -----------------------
-// dns hijjacking 
+// DNS hijacking
 // -----------------------
 
 DNSServer dnsServer;
 const byte DNS_PORT = 53;
-  
+
+
 // -----------------------
 // painlessMesh settings
 // -----------------------
 
-#define MESH_PREFIX     "mesh" // the wifi ssid needs to be the same for each node
+#define MESH_PREFIX     "mesh"
 #define MESH_PASSWORD   "12345678"
 #define MESH_PORT       5555
- 
-#define CHUNK_SIZE 256 // files need to be transfered from the file system to RAM before sending, so we need to chunk larger files to not overflow the RAM.
+
+#define CHUNK_SIZE 128
+
+
+
+// -----------------------
+// Incoming file
+// -----------------------
+
+bool receivingFile=false; //transfer lock
 
 struct IncomingFile
 {
-  String name;
-  size_t size;
-  size_t received;
-  File file;
+    String name;
+    size_t size;
+    size_t received;
+    File file;
 };
 
 IncomingFile incoming;
 
+
+// -----------------------
+// Incoming file queue
+// -----------------------
+
+
+struct FileRequest
+{
+    uint32_t node;
+    String filename;
+};
+
+std::vector<FileRequest> sendQueue;
+
+// -----------------------
+// Globals
+// -----------------------
 
 painlessMesh mesh;
 
@@ -47,77 +73,77 @@ AsyncWebServer server(80);
 
 Scheduler userScheduler;
 
+ 
+// -----------------------
+// File transfer state
+// -----------------------
+
+  uint8_t sendBuffer[CHUNK_SIZE];
+  
+  File outgoingFile;
+  
+  uint32_t outgoingNode;
+  
+  String outgoingFilename;
+  
+  bool sendingFile = false;
+  
+  size_t outgoingPos = 0;
+
+
 
 // -----------------------
-// Has to prevent dupes
+// TaskScheduler file sender
+// Must be global
 // -----------------------
+
+void sendFileChunk();
+
+
+Task fileTask(
+    300, //slowwwwwww
+    TASK_FOREVER,
+    []()
+    {
+        sendFileChunk();
+    }
+);
+
+
+
+
+// -----------------------
+// Helpers
+// -----------------------
+
 bool hasFile(String name)
 {
     return LittleFS.exists(name);
 }
 
 
+
 // -----------------------
-// Base64 en/decoder (for images etc)
+// Base64
 // -----------------------
 
 const char b64chars[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 
-int base64Decode(
-    String input,
-    uint8_t *output
+
+void base64Encode(
+    uint8_t *data,
+    size_t len,
+    String &output
 )
 {
-    int val = 0;
-    int valb = -8;
-    int outLen = 0;
-
-    for (int i = 0; i < input.length(); i++)
-    {
-        char c = input[i];
-
-        if (c == '=')
-            break;
-
-        int idx = -1;
-
-        for(int j=0;j<64;j++)
-        {
-            if(b64chars[j] == c)
-            {
-                idx=j;
-                break;
-            }
-        }
-
-        if(idx < 0)
-            continue;
-
-        val = (val << 6) + idx;
-        valb += 6;
-
-        if(valb >= 0)
-        {
-            output[outLen++] =
-                (val >> valb) & 0xFF;
-
-            valb -= 8;
-        }
-    }
-
-    return outLen;
-}
-
-
-String base64Encode(uint8_t *data, size_t len)
-{
-    String output;
+    output = "";
 
     int i = 0;
 
-    while (i < len)
+
+    while(i < len)
     {
         uint32_t octet_a = i < len ? data[i++] : 0;
         uint32_t octet_b = i < len ? data[i++] : 0;
@@ -129,21 +155,189 @@ String base64Encode(uint8_t *data, size_t len)
             (octet_b << 8) |
             octet_c;
 
+
+
         output += b64chars[(triple >> 18) & 0x3F];
         output += b64chars[(triple >> 12) & 0x3F];
 
-        if (i - 2 < len)
+
+        if(i - 2 < len)
             output += b64chars[(triple >> 6) & 0x3F];
         else
             output += '=';
 
-        if (i - 1 < len)
+
+
+        if(i - 1 < len)
             output += b64chars[triple & 0x3F];
         else
             output += '=';
     }
+}
 
-    return output;
+
+
+
+int base64Decode(
+    String input,
+    uint8_t *output
+)
+{
+    int val = 0;
+    int valb = -8;
+    int outLen = 0;
+
+
+    for(int i = 0; i < input.length(); i++)
+    {
+        char c = input[i];
+
+
+        if(c == '=')
+            break;
+
+
+        int idx = -1;
+
+
+        for(int j = 0; j < 64; j++)
+        {
+            if(b64chars[j] == c)
+            {
+                idx = j;
+                break;
+            }
+        }
+
+
+        if(idx < 0)
+            continue;
+
+
+        val = (val << 6) + idx;
+        valb += 6;
+
+
+        if(valb >= 0)
+        {
+            output[outLen++] =
+                (val >> valb) & 0xFF;
+
+            valb -= 8;
+        }
+    }
+
+
+    return outLen;
+}
+
+
+
+
+// -----------------------
+// Send File Chunk
+// -----------------------
+
+void sendFileChunk()
+{
+
+    if(!sendingFile)
+        return;
+
+    
+    // guard heap, check before attempting file transfers
+    if(ESP.getFreeHeap() < 20000)
+    {
+        Serial.println("LOW HEAP - waiting");
+        return;
+    }
+
+    Serial.print("Heap: ");
+    Serial.println(ESP.getFreeHeap());
+
+    Serial.print("Offset: ");
+    Serial.println(outgoingPos);
+    
+    if(!outgoingFile.available())
+    {
+
+        outgoingFile.close();
+
+
+        String endMsg;
+        endMsg.reserve(64);
+
+
+        endMsg = "FILE_END:";
+        endMsg += outgoingFilename;
+
+
+
+        mesh.sendSingle(
+            outgoingNode,
+            endMsg
+        );
+
+
+
+        outgoingFilename = "";
+
+        sendingFile = false;
+
+
+        Serial.println("file sent");
+
+
+        return;
+    }
+
+
+
+
+
+    int len =
+        outgoingFile.read(
+            sendBuffer,
+            sizeof(sendBuffer)
+        );
+
+
+
+    String encoded;
+    encoded.reserve(180);
+
+
+
+    base64Encode(
+        sendBuffer,
+        len,
+        encoded
+    );
+
+
+
+    
+    String chunk;
+    chunk.reserve(430);
+    
+    
+    chunk = "FILE_CHUNK:";
+    chunk += outgoingFilename;
+    chunk += ":";
+    chunk += outgoingPos;
+    chunk += ":";
+    chunk += encoded;
+
+
+
+    mesh.sendSingle(
+        outgoingNode,
+        chunk
+    );
+
+
+
+    outgoingPos += len;
 }
 
 // -----------------------
@@ -153,217 +347,418 @@ String base64Encode(uint8_t *data, size_t len)
 void receivedCallback(uint32_t from, String &msg)
 {
 
-Serial.printf("RX %u: %s\n",from,msg.c_str());
-
-
-if(msg=="FILE_LIST")
-{
-    Dir dir = LittleFS.openDir("/");
-
-    while(dir.next())
+    Serial.print("RX ");
+    Serial.print(from);
+    Serial.print(": ");
+    
+    if(msg.length()>50)
     {
-        String reply="FILE:";
-        reply += dir.fileName();
-        reply += ":";
-        reply += dir.fileSize();
-
-        mesh.sendSingle(from,reply);
-    }
-
-    return;
-}
-
-
-if(msg.startsWith("FILE:"))
-{
-
-    int p1 = msg.indexOf(':',5);
-
-    String filename =
-        msg.substring(5,p1);
-
-    size_t filesize =
-        msg.substring(p1+1).toInt();
-
-    Serial.print("Remote file: ");
-    Serial.print(filename);
-    Serial.print(" ");
-    Serial.println(filesize);
-
-    if(!hasFile(filename))
-    {
-
-        Serial.println("Missing, requesting");
-
-        mesh.sendSingle(
-            from,
-            "GET_FILE:" + filename
-        );
-
+        Serial.println(msg.substring(0,50));
     }
     else
     {
-        Serial.println("Already have it");
+        Serial.println(msg);
     }
 
-    return;
-}
 
-if(msg.startsWith("GET_FILE:"))
-{
-    String filename=msg.substring(9);
+    // -----------------------
+    // FILE LIST REQUEST
+    // -----------------------
 
-    File f=LittleFS.open(filename,"r");
-
-    if(!f)
+    if(msg == "FILE_LIST")
     {
-        Serial.println("missing file");
+
+        Dir dir = LittleFS.openDir("/");
+
+
+        while(dir.next())
+        {
+        
+            String filename = dir.fileName();
+        
+            if(filename.endsWith(".tmp"))
+            {
+                continue;
+            }
+        
+        
+            String reply;
+            reply.reserve(80);
+        
+        
+            reply = "FILE:";
+            reply += filename;
+            reply += ":";
+            reply += dir.fileSize();
+        
+        
+            mesh.sendSingle(
+                from,
+                reply
+            );
+        }
+
+
         return;
     }
 
 
-    String start="FILE_START:";
-    start+=filename;
-    start+=":";
-    start+=f.size();
 
-    mesh.sendSingle(from,start);
 
-    size_t pos=0;
 
-    while(f.available())
+    // -----------------------
+    // FILE ANNOUNCEMENT
+    // -----------------------
+
+    if(msg.startsWith("FILE:"))
     {
-        uint8_t buffer[CHUNK_SIZE];
-        
-        int len = f.read(buffer, CHUNK_SIZE);
-        
-        String chunk="FILE_CHUNK:";
-        chunk += filename;
-        chunk += ":";
-        chunk += pos;
-        chunk += ":";
-        
-        String encoded = base64Encode(buffer, len);
-        
-        chunk += encoded;
-        
-        mesh.sendSingle(from, chunk);
 
-        pos+=len;
+        int p1 = msg.indexOf(':',5);
 
-        delay(5);
+
+        String filename =
+            msg.substring(5,p1);
+
+
+
+        size_t filesize =
+            msg.substring(p1+1).toInt();
+
+
+
+        Serial.print("Remote file: ");
+        Serial.print(filename);
+        Serial.print(" ");
+        Serial.println(filesize);
+
+
+
+        if(!hasFile(filename) && !receivingFile)
+        {
+
+            String request;
+            request.reserve(80);
+
+
+            request = "GET_FILE:";
+            request += filename;
+
+
+            mesh.sendSingle(
+                from,
+                request
+            );
+
+        }
+        else
+        {
+            Serial.println("Already have it");
+        }
+
+
+        return;
     }
 
 
-    f.close();
-
-    String end="FILE_END:";
-    end+=filename;
-
-    mesh.sendSingle(from,end);
-
-    Serial.println("file sent");
-
-    return;
-}
 
 
-if(msg.startsWith("FILE_START:"))
-{
-    int p1=msg.indexOf(':',11);
-
-    incoming.name=
-        msg.substring(11,p1);
-
-    incoming.size=
-        msg.substring(p1+1).toInt();
-
-    incoming.received=0;
-
-    incoming.file=
-        LittleFS.open(
-            incoming.name,
-            "w"
-        );
-
-    Serial.println("Receiving:");
-    Serial.println(incoming.name);
-
-    return;
-}
 
 
-if(msg.startsWith("FILE_CHUNK:"))
-{
+    // -----------------------
+    // GET FILE
+    // -----------------------
 
-    int p1 = msg.indexOf(':',11);
-    int p2 = msg.indexOf(':',p1+1);
-
-    String filename =
-        msg.substring(11,p1);
-
-    int offset =
-        msg.substring(p1+1,p2).toInt();
-
-    String encoded =
-        msg.substring(p2+1);
-
-    uint8_t decoded[400];
-
-    int decodedLen =
-        base64Decode(
-            encoded,
-            decoded
-        );
-
-    if(incoming.file)
+    if(msg.startsWith("GET_FILE:"))
     {
-        incoming.file.seek(offset);
 
-        incoming.file.write(
-            decoded,
-            decodedLen
-        );
-
-        incoming.received += decodedLen;
-    }
-
-    return;
-}
+        String filename =
+            msg.substring(9);
 
 
-  if(msg.startsWith("FILE_END:"))
-  {
-  
-      if(incoming.file)
+      if(sendingFile)
       {
-          incoming.file.close();
+          Serial.println("Already sending");
+          return;
       }
-  
-      Serial.println("Finished:");
-      Serial.println(incoming.name);
-  
-      // tell the whole mesh we now have this file
-      mesh.sendBroadcast(
-          "NEW_FILE:" + incoming.name
-      );
-  
-      return;
-  }
 
-  if(msg.startsWith("NEW_FILE:"))
+        outgoingFile =
+            LittleFS.open(
+                filename,
+                "r"
+            );
+
+
+        if(!outgoingFile)
+        {
+            Serial.println("missing file");
+            return;
+        }
+
+
+
+        outgoingNode = from;
+
+        outgoingFilename = filename;
+
+        outgoingPos = 0;
+
+
+
+        String start;
+        start.reserve(100);
+
+
+        start = "FILE_START:";
+        start += filename;
+        start += ":";
+        start += outgoingFile.size();
+
+
+
+        mesh.sendSingle(
+            from,
+            start
+        );
+
+
+
+        sendingFile = true;
+
+
+        Serial.println("File transfer queued");
+
+
+        return;
+    }
+
+
+
+
+
+
+    // -----------------------
+    // FILE START
+    // -----------------------
+
+    if(msg.startsWith("FILE_START:"))
     {
-        String filename = msg.substring(9);
-    
+
+        int p1 =
+            msg.indexOf(':',11);
+
+
+
+        incoming.name =
+            msg.substring(11,p1);
+
+
+
+        incoming.size =
+            msg.substring(p1+1).toInt();
+
+
+        incoming.received = 0;
+        receivingFile=true;
+
+
+
+        String tempName;
+
+        tempName.reserve(80);
+        
+        tempName = incoming.name;
+        
+        if(!tempName.endsWith(".tmp"))
+        {
+            tempName += ".tmp";
+        }
+
+
+
+        incoming.file =
+            LittleFS.open(
+                tempName,
+                "w"
+            );
+
+
+
+        Serial.println("Receiving:");
+        Serial.println(incoming.name);
+
+
+
+        return;
+    }
+
+
+
+
+
+
+
+    // -----------------------
+    // FILE CHUNK
+    // -----------------------
+
+    if(msg.startsWith("FILE_CHUNK:"))
+    {
+
+
+        int p1 =
+            msg.indexOf(':',11);
+
+
+        int p2 =
+            msg.indexOf(':',p1+1);
+
+
+
+        int offset =
+            msg.substring(
+                p1+1,
+                p2
+            ).toInt();
+
+
+
+        String encoded =
+            msg.substring(p2+1);
+
+
+
+        uint8_t decoded[400];
+
+
+
+        int decodedLen =
+            base64Decode(
+                encoded,
+                decoded
+            );
+
+
+
+        if(incoming.file)
+        {
+
+            incoming.file.seek(offset);
+
+
+            incoming.file.write(
+                decoded,
+                decodedLen
+            );
+
+
+            incoming.received += decodedLen;
+        }
+
+
+
+        return;
+    }
+
+
+
+
+
+
+
+    // -----------------------
+    // FILE END
+    // -----------------------
+
+    if(msg.startsWith("FILE_END:"))
+    {
+
+        if(incoming.file)
+        {
+            incoming.file.close();
+
+            if(LittleFS.exists(incoming.name))
+            {
+                LittleFS.remove(incoming.name);
+            }
+
+            String tempName;
+
+            tempName.reserve(80);
+
+            tempName = incoming.name;
+            
+            if(!tempName.endsWith(".tmp"))
+            {
+                tempName += ".tmp";
+            }
+
+            LittleFS.rename(
+                tempName,
+                incoming.name
+            );
+        }
+
+        Serial.println("Finished:");
+        Serial.println(incoming.name);
+
+        receivingFile=false;
+        String announce;
+
+        announce.reserve(80);
+
+        if(!incoming.name.endsWith(".tmp"))
+        {
+            announce = "NEW_FILE:";
+            announce += incoming.name;
+        
+            mesh.sendBroadcast(
+                announce
+            );
+        }
+
+//        mesh.sendBroadcast(
+//            announce
+//        );
+
+
+        return;
+       
+    }
+
+
+
+
+
+
+
+    // -----------------------
+    // NEW FILE
+    // -----------------------
+
+    if(msg.startsWith("NEW_FILE:"))
+    {
+
+        String filename =
+            msg.substring(9);
+
+
+
         Serial.println("New file announced:");
         Serial.println(filename);
-    
-        mesh.sendBroadcast("FILE_LIST");
-    
+
+
+
+        mesh.sendBroadcast(
+            "FILE_LIST"
+        );
+
+
+
         return;
     }
 
 }
+
 // -----------------------
 // Upload handler
 // -----------------------
@@ -403,36 +798,6 @@ void handleUpload(
 
     Serial.println("Upload finished");
   }
-}
-
-///debug send file helper
-void sendFile(uint32_t destination, String filename)
-{
-  File file = LittleFS.open(filename, "r");
-
-  if(!file)
-  {
-    Serial.println("File not found");
-    return;
-  }
-
-
-  String content = file.readString();
-
-  file.close();
-
-  String msg = "FILE:";
-  msg += filename;
-  msg += ":";
-  msg += content;
-
-  mesh.sendSingle(
-    destination,
-    msg
-  );
-
-  Serial.println("Sent:");
-  Serial.println(msg);
 }
 
 // -----------------------
@@ -475,18 +840,10 @@ void setup()
 
   mesh.setDebugMsgTypes(
     ERROR |
-    STARTUP |
-    CONNECTION 
+    STARTUP
   );
 
 
-//  mesh.init(
-//    MESH_PREFIX,
-//    MESH_PASSWORD,
-//    MESH_PORT,
-//    WIFI_AP,
-//    6
-//  );
 
   mesh.init(
       MESH_PREFIX,
@@ -496,6 +853,9 @@ void setup()
   );
   
 
+
+userScheduler.addTask(fileTask);
+fileTask.enable();
   
   Serial.println("Mesh started");
 
@@ -691,67 +1051,43 @@ void loop()
   mesh.update();
   dnsServer.processNextRequest();
 
-  //userScheduler.execute();
-
-//  static uint32_t lastRequest = 0;
-//  
-//  if(mesh.getNodeList().size()>0 &&
-//     millis()-lastRequest > 30000) // request every 30 seconds
-//  {
-//      lastRequest = millis();
-//  
-//      uint32_t node =
-//        mesh.getNodeList().front();
-//  
-//      mesh.sendSingle(
-//        node,
-//        "GET:/newfile1.txt"
-//      );
-//  }
-
-static unsigned long syncTimer=0;
+  userScheduler.execute(); //send file chunks
 
 
-    if(millis()-syncTimer>30000)
-    {
-        syncTimer=millis();
-    
-    
-        if(mesh.getNodeList().size()>0)
-        {
-    
-//            uint32_t node =
-//                mesh.getNodeList().front();
-//    
-//    
-//            mesh.sendSingle(
-//                node,
-//                "FILE_LIST"
-//            );
-            mesh.sendBroadcast("FILE_LIST");
-    
-            Serial.println("Requesting file list");
-    
-        }
-    }
+  static unsigned long syncTimer=0;
 
-    static unsigned long last = 0;
-  
-    if (millis() - last > 3000)
-    {
-      last = millis();
-  
-      Serial.println("----");
-      Serial.print("Node ID: ");
-      Serial.println(mesh.getNodeId());
-  
-      Serial.print("Connections: ");
-      Serial.println(mesh.getNodeList().size());
-  
-      for(auto node : mesh.getNodeList())
+
+  if(!sendingFile && !receivingFile)
+  {
+      if(millis()-syncTimer>30000)
       {
-        Serial.print("Connected: ");
-        Serial.println(node);
+          syncTimer=millis();
+  
+          if(mesh.getNodeList().size()>0)
+          {
+              mesh.sendBroadcast("FILE_LIST"); 
+              Serial.println("Requesting file list");
+          }
       }
-    }
+  }
+
+//    static unsigned long last = 0;
+//  
+//    if (millis() - last > 3000)
+//    {
+//      last = millis();
+//  
+//      Serial.println("----");
+//      Serial.print("Node ID: ");
+//      Serial.println(mesh.getNodeId());
+//  
+//      Serial.print("Connections: ");
+//      Serial.println(mesh.getNodeList().size());
+//  
+//      for(auto node : mesh.getNodeList())
+//      {
+//        Serial.print("Connected: ");
+//        Serial.println(node);
+//      }
+//    }
 }
